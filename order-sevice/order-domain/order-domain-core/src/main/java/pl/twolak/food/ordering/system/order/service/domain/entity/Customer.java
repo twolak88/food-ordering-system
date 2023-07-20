de32b8541e37fc233b47b42b49f8afd2edfd113e
Tomasz Wolak <twolak88@gmail.com>
2023-07-20 20:00:47 +0200

    EURO-557 - food ordering system - implement Order Entity state changing methods

---
+++ order-sevice/order-domain/order-domain-core/src/main/java/pl/twolak/food/ordering/system/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package pl.twolak.food.ordering.system.order.service.domain.entity;
+
+import pl.twolak.food.ordering.system.domain.entity.AggregateRoot;
+import pl.twolak.food.ordering.system.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
